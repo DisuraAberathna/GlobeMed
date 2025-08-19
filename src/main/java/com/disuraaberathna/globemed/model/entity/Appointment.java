f93164547858de7c9648f3e533dc45757675e7e7
Disura Aberathna <disura2005@gmail.com>
2025-08-19 19:20:16 +0530

    appointment entity updated

--- src/main/java/com/disuraaberathna/globemed/model/entity/Appointment.java
+++ src/main/java/com/disuraaberathna/globemed/model/entity/Appointment.java
@@ -1,8 +1,10 @@
 package com.disuraaberathna.globemed.model.entity;
 
+import com.disuraaberathna.globemed.enums.Status;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.Date;
 
 @Entity
 @Table(name = "appointments")
@@ -10,5 +12,63 @@ public class Appointment implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "doctor_id", nullable = false)
+    private User doctor;
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "patient_id", nullable = false)
+    private Patient patient;
+    private Date dateTime;
+    @Enumerated(EnumType.STRING)
+    private Status status;
 
+    public Appointment() {
+    }
+
+    public Appointment(User doctor, Patient patient, Date dateTime, Status status) {
+        this.doctor = doctor;
+        this.patient = patient;
+        this.dateTime = dateTime;
+        this.status = status;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(User doctor) {
+        this.doctor = doctor;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Date getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(Date dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 }
