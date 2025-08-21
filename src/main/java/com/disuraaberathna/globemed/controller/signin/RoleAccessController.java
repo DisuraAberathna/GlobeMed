ebce37b26f7e7543532ca913ad977e2a13cbabcb
Disura Aberathna <disura2005@gmail.com>
2025-08-21 20:19:53 +0530

    role access controller created

---
+++ src/main/java/com/disuraaberathna/globemed/controller/signin/RoleAccessController.java
@@ -0,0 +1,12 @@
+package com.disuraaberathna.globemed.controller.signin;
+
+import com.disuraaberathna.globemed.model.dao.SignInDAO;
+
+public class RoleAccessController extends SignInController {
+    @Override
+    public SignInDAO handleRequest(SignInDAO signInDAO) {
+        signInDAO.setUserRole(signInDAO.getUser().getRole());
+        signInDAO.setSuccess(true);
+        return signInDAO;
+    }
+}
