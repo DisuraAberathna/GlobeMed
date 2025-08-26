17e1f0200e4dace9142f8a704e7e5b0c1dd76589
Disura Aberathna <disura2005@gmail.com>
2025-08-26 17:30:41 +0530

    create LoggingSignInDecorator

---
+++ src/main/java/com/disuraaberathna/globemed/controller/signin/LoggingSignInDecorator.java
@@ -0,0 +1,30 @@
+package com.disuraaberathna.globemed.controller.signin;
+
+import com.disuraaberathna.globemed.model.dao.SignInDAO;
+import com.disuraaberathna.globemed.util.LoggerUtil;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class LoggingSignInDecorator extends SignInControllerDecorator {
+    private static final Logger logger = LoggerUtil.getLogger();
+
+    public LoggingSignInDecorator(SignInController signInController) {
+        super(signInController);
+    }
+
+    @Override
+    public SignInDAO handleRequest(SignInDAO signInDAO) {
+        logger.log(Level.INFO, "Sign in attempt for user : " + signInDAO.getUsername());
+
+        SignInDAO response = super.handleRequest(signInDAO);
+
+        if (response.isSuccess()) {
+            logger.log(Level.INFO, "Sign in success for user : " + signInDAO.getUsername());
+        } else {
+            logger.log(Level.WARNING, "Sign in failed for user : " + signInDAO.getUsername());
+        }
+
+        return response;
+    }
+}
