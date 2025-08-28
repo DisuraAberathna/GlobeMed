ac6007623be6bb4b28e7baf2b0b964478f4668ad
Disura Aberathna <disura2005@gmail.com>
2025-08-28 19:57:39 +0530

    Update PatientController.java

--- src/main/java/com/disuraaberathna/globemed/controller/PatientController.java
+++ src/main/java/com/disuraaberathna/globemed/controller/PatientController.java
@@ -1,4 +1,27 @@
 package com.disuraaberathna.globemed.controller;
 
+import com.disuraaberathna.globemed.model.dao.PatientDAO;
+import com.disuraaberathna.globemed.util.LoggerUtil;
+import com.disuraaberathna.globemed.view.PatientView;
+
+import javax.swing.*;
+import java.util.logging.Logger;
+
 public class PatientController {
+    private final PatientView view;
+    private final PatientDAO patientDAO;
+    private final JPanel viewPanel;
+    private final static Logger logger = LoggerUtil.getLogger();
+
+    public PatientController(PatientView view, PatientDAO patientDAO, JPanel viewPanel) {
+        this.view = view;
+        this.patientDAO = patientDAO;
+        this.viewPanel = viewPanel;
+    }
+
+    public void showView() {
+        viewPanel.removeAll();
+        viewPanel.add(view);
+        SwingUtilities.updateComponentTreeUI(viewPanel);
+    }
 }
